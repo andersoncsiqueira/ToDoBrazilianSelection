d8f087339ba2b83934d67903c6c9d9e1cf716a12
andersoncsiqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2021-12-13 06:28:21 -0300

    2

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const insertPlayer = document.querySelector('.addPlayer')
-
+const container = document.querySelector('.containNames')
 
 insertPlayer.addEventListener('submit', event => {
     event.preventDefault()
-    console.log(event.target.value)
+    const inputValue = event.target.add.value.trim()
+
+    if(inputValue.length){
+        container.innerHTML += `
+    <li>
+        <span> ${inputValue}</span>
+        <i class="delete">x</i>
+    </li>`
+    }
+    event.target.reset()
 })
